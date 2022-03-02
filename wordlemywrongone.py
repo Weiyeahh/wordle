6c04b2d9febf4978700901569175df6b53e2e854
Weiyeahh <wzhang5@babson.edu>
2022-03-01 19:10:08 -0500

    hw

--- wordlemywrongone.py
+++ wordlemywrongone.py
@@ -13,22 +13,24 @@ lightgreen=(153,255,204)
 
 word=random.choice(wordlist).upper()
 print(word)
-def check(word):
-    gridcolor=[grey,grey,grey,grey,grey]
-    for x in range(0,4):
+window = pygame.display.set_mode((600, 800))
+win=False
+window.fill(black)
+font = pygame.font.SysFont("Helvetica neue", 40)
+for x in range(0,4):
         for y in range(0,4):
             pygame.draw.rect(window, grey, pygame.Rect(60+(x*80), 50+(y*80), 50, 50),2)
-
-    pygame.display.set_caption("Wordle!")
-    font = pygame.font.SysFont("Helvetica neue", 40)
-    window.blit(font.render(guess, True, grey), (180, 530))
-    spacing=0
-    for turn in (0,4):
-        for x in range(0,4):
-            pygame.draw.rect(window, gridcolor[x], pygame.Rect(60 +spacing, 50+ (turn*80), 50, 50))
-            window.blit(font.render(guess[x], True, black), (70 + spacing, 50 + (turn*80)))
-            spacing+=80
-
+gridcolor=[grey,grey,grey,grey,grey]
+guess=""
+spacing=0
+for turn in (0,4):
+    for x in range(0,4):
+        pygame.draw.rect(window, gridcolor[x], pygame.Rect(60 +spacing, 50+ (turn*80), 50, 50))
+        window.blit(font.render(guess[x], True, black), (70 + spacing, 50 + (turn*80)))
+        spacing+=80
+pygame.display.set_caption("Wordle!")
+window.blit(font.render(guess, True, grey), (180, 530))
+def check(word):
     for x in range(0,4):
         if guess[x] in word:
             gridcolor[x]=yellow
@@ -37,16 +39,13 @@ def check(word):
     if gridcolor==[green,green,green,green,green]:
         return True
     
-guess=""
-window = pygame.display.set_mode((600, 800))
-win=False
-window.fill(black)
+
+
 def getinput():
     turn=0
     while True:
         while turn<=5:
             for event in pygame.event.get():
-                win=check(word)
                 if event.type == pygame.KEYDOWN:
                     guess+=event.unicode.upper()
                     if event.key == K_RETURN:
@@ -64,7 +63,7 @@ def getinput():
                             pygame.display.update()
                     if event.key == pygame.K_BACKSPACE:
                         guess = guess[:-1]
-                    
+                    win=check(word)
         window.blit("You ran out of chances",(90,200))
         pygame.display.update()
 
