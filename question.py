9619c6fe6d0991a61488b53d48235cff13847464
Weiyeahh <wzhang5@babson.edu>
2022-03-05 14:45:38 -0500

    Create question.py

---
+++ question.py
@@ -0,0 +1,14 @@
+import math
+保质期=math.inf
+yeung=""
+def what_is_it():
+    t=3 #weeks
+    while t<=保质期:
+        if yeung=="男朋友":
+            t+=1   
+        else:
+            break
+        return True
+#q1. for the function to return True, what would the initial variable "yeung" be?  
+
+
